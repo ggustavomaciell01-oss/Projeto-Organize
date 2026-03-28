7224d243b78ea1c6762fa64fee8b573718963803
ggustavomaciell01-oss <ggustavomaciell01@gmail.com>
2026-03-28 01:45:12 -0400

    commit frontend
    ajustes

--- frontend/src/app/dashboard/page.js
+++ frontend/src/app/dashboard/page.js
@@ -1,24 +1,15 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
 
 export default function Dashboard() {
 
-  const router = useRouter();
   const [nome, setNome] = useState("");
 
   useEffect(() => {
 
-    const logado =
-      localStorage.getItem("logado");
-
     const nomeUser =
       localStorage.getItem("nome");
-
-    if (!logado) {
-      router.push("/login");
-    }
 
     if (nomeUser) {
       setNome(nomeUser);
@@ -26,33 +17,19 @@ export default function Dashboard() {
 
   }, []);
 
-  function logout() {
-
-    localStorage.removeItem("logado");
-    localStorage.removeItem("nome");
-
-    router.push("/login");
-
-  }
-
   return (
 
-    <main className="min-h-screen bg-black text-white p-10">
+    <main className="min-h-screen px-6 py-10">
 
-      <h1 className="text-4xl font-bold mb-4">
+      <h1 className="text-3xl font-bold mb-2">
         Dashboard
       </h1>
 
-      <h2 className="text-xl mb-6">
-        👋 Olá, {nome}
-      </h2>
+      <p className="text-gray-400 mb-10">
+        Bem-vindo, {nome}
+      </p>
 
-      <button
-        onClick={logout}
-        className="bg-white text-black px-6 py-2 rounded-lg hover:bg-gray-200 transition"
-      >
-        Sair
-      </button>
+
 
     </main>
 

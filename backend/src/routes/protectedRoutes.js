59f8efbbd5712c8c0f46e6a19c16236b5da4d839
ggustavomaciell01-oss <ggustavomaciell01@gmail.com>
2026-04-15 23:24:10 -0400

    back 100%

---
+++ backend/src/routes/protectedRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import auth from "../lib/auth.js";
+
+const router = express.Router();
+
+// Rota protegida
+router.get(
+  "/protected",
+  auth.handler,
+  (req, res) => {
+    res.json({
+      message: "Rota protegida funcionando ",
+    });
+  }
+);
+
+export default router;

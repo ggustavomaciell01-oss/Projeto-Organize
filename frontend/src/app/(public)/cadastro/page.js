c54a470bd1134584a235771dd5a490112602061c
gabi346 <bih45qlima@gmail.com>
2026-06-03 10:50:19 -0400

    atualizar o que ja fiz

--- frontend/src/app/(public)/cadastro/page.js
+++ frontend/src/app/(public)/cadastro/page.js
@@ -1,10 +1,10 @@
-import Cadrastrar from "@/components/ui/cadastrar";
+import RegisterForm from "@/components/ui/cadastrar";
 
-export default function Cadastrar() {
+export default function Cadastro() {
   return (
     <div className="flex min-h-[calc(100vh-5rem)] flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <Cadastrar />
+        <RegisterForm />
       </div>
     </div>
   );
--- frontend/src/app/(public)/login/page.js
+++ frontend/src/app/(public)/login/page.js
@@ -1,10 +1,10 @@
 import LoginForm from "@/components/ui/login-form";
 
-export default function LoginForms() {
+export default function Login() {
   return (
     <div className="flex min-h-[calc(100vh-5rem)] flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <LoginForms />
+        <LoginForm />
       </div>
     </div>
   );

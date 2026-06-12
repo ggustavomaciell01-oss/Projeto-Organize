028f9baa163b2efb2c46f7fc3aa0a3b3feab8816
gabi346 <bih45qlima@gmail.com>
2026-06-12 18:06:03 -0400

    .

--- frontend/src/proxy.js
+++ frontend/src/proxy.js
@@ -1,7 +1,8 @@
+// frontend\src\middleware.js
 import { NextResponse } from "next/server";
 
 // Lista de rotas que precisam de login
-const rotasPrivadas = ["/dashboard"];
+const rotasPrivadas = ["/dashboard", "/links"];
 
 // Lista de rotas que NÃO devem ser acessadas se já estiver logado
 const rotasDeAuth = ["/login", "/cadastro"];
@@ -10,11 +11,14 @@ export async function proxy(request) {
   const { pathname } = request.nextUrl;
 
   // Verifica a sessão perguntando ao backend
-  const sessionResponse = await fetch("http://localhost:5500/api/auth/get-session", {
-    headers: {
-      cookie: request.headers.get("cookie") ?? "",
+  const sessionResponse = await fetch(
+    "http://localhost:5500/api/auth/get-session",
+    {
+      headers: {
+        cookie: request.headers.get("cookie") ?? "",
+      },
     },
-  });
+  );
 
   const session = await sessionResponse.json();
   const estaLogado = !!session?.user;
@@ -34,5 +38,5 @@ export async function proxy(request) {
 
 // Define em quais rotas o middleware roda
 export const config = {
-  matcher: ["/dashboard/:path*", "/login", "/cadastro"],
+  matcher: ["/dashboard/:path*", "/relatorio/:path*", "/login", "/cadastro"],
 };
